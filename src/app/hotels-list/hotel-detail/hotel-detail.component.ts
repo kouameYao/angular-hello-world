05a02b13dd7b65b408a0e3c62ca289e4025a03af
kouameYao <yaojean0857@gmail.com>
2021-10-04 16:46:04 +0000

    28 : getting params via Navigation

--- src/app/hotels-list/hotel-detail/hotel-detail.component.ts
+++ src/app/hotels-list/hotel-detail/hotel-detail.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IHotel } from '../hotel';
+import { HotelListService } from '../hotel-list.service';
 
 @Component({
   selector: 'app-hotel-detail',
   templateUrl: './hotel-detail.component.html',
-  styleUrls: ['./hotel-detail.component.css']
+  styleUrls: ['./hotel-detail.component.css'],
 })
 export class HotelDetailComponent implements OnInit {
-
-  constructor() { }
+  public hotel: any = {};
+  // public hotel: IHotel = <IHotel>{};
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private hotelService: HotelListService
+  ) {}
 
   ngOnInit(): void {
+    const id: number = Number(this.route.snapshot.paramMap.get('id'));
+    console.log(id);
+
+    this.hotelService.getHotels().subscribe((hotels: IHotel[]) => {
+      this.hotel = hotels.find((hotel) => (hotel.hotelId = id));
+      console.log(this.hotel);
+    });
   }
 
+  public backToList(): void {
+    this.router.navigate(['/hotels']);
+  }
 }
+
+// this.hotelService.getHotels().subscribe((hotels: IHotel[]) => {
+//   this.hotel = hotels.find(hotel => hotel.hotelId === id)
+// }).
