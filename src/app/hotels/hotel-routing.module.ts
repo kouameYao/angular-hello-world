f7769ce4aa23589270f0686ef85fe83d9b83c392
kouameYao <yaojean0857@gmail.com>
2021-10-05 08:28:58 +0000

    33 : routing module

---
+++ src/app/hotels/hotel-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HotelDetailComponent } from './hotel-detail/hotel-detail.component';
+import { HotelDetailGuard } from './shared/guards/hotel-detail.guard';
+import { HotelListComponent } from './hotel-list/hotel-list.component';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      {
+        path: 'hotels/:id',
+        component: HotelDetailComponent,
+        canActivate: [HotelDetailGuard],
+      },
+      { path: 'hotels', component: HotelListComponent },
+    ]),
+  ],
+  exports: [RouterModule],
+})
+export class HotelRoutingModule {}
--- src/app/hotels/hotel.module.ts
+++ src/app/hotels/hotel.module.ts
@@ -1,23 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 import { HotelListComponent } from './hotel-list/hotel-list.component';
 import { HotelDetailComponent } from './hotel-detail/hotel-detail.component';
-import { HotelDetailGuard } from './shared/guards/hotel-detail.guard';
 import { SharedModule } from '../shared/shared.module';
+import { HotelRoutingModule } from './hotel-routing.module';
+import { HotelEditComponent } from './hotel-edit/hotel-edit.component';
 
 @NgModule({
-  declarations: [HotelListComponent, HotelDetailComponent],
-  imports: [
-    RouterModule.forChild([
-      {
-        path: 'hotels/:id',
-        component: HotelDetailComponent,
-        canActivate: [HotelDetailGuard],
-      },
-      { path: 'hotels', component: HotelListComponent },
-    ]),
-    SharedModule,
-  ],
+  declarations: [HotelListComponent, HotelDetailComponent, HotelEditComponent],
+  imports: [SharedModule, HotelRoutingModule],
 })
 export class HotelModule {}
